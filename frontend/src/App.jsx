6055ec23c8281e4d5cd7d78f7b73d4a3d87472b4
Pawan Yadav <pawanydv3010@gmail.com>
2025-09-26 16:20:03 +0530

    react-icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import LandingPage from './components/Landingpage';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <div className="App">
       <Navbar />
+      <LandingPage/>
     </div>
     </>
   )
